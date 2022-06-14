92edea7d72778f5ff34d3a6bccaed80d08a7ac52
Samvel Belyan <Samvel_Belyan@epam.com>
2022-06-14 11:16:10 +0400

    Remove ignored files

--- Persistence/Models/Cart.cs
+++ Persistence/Models/Cart.cs
@@ -1,10 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Persistence.Models;
+﻿namespace Persistence.Models;
 
 public class Cart
 {
-    [Required]
     public string Id { get; set; }
     public IList<Item> Items { get; set; } = new List<Item>();
 }
--- Persistence/Models/Item.cs
+++ Persistence/Models/Item.cs
@@ -1,19 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Persistence.Models;
 
-namespace Persistence.Models;
 public class Item
 {
-    [Required]
     public int Id { get; set; }
-    [Required]
     public string Name { get; set; }
     public Image Image { get; set; }
-    [Required]
     public decimal Price { get; set; }
     public int Quantity { get; set; }
 }
